17a7edc9996c71bb444913362975d36a8eb9f152
cucea <jjroblesg@hotmail.com>
2024-03-06 20:28:12 -0600

    código terminado

--- app/src/main/java/com/example/divisas/MainActivity.kt
+++ app/src/main/java/com/example/divisas/MainActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
@@ -19,11 +21,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewModelScope
 import com.example.divisas.ui.theme.DivisasTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,32 +52,46 @@ class MainActivity : ComponentActivity() {
         var contenidoWebService by remember { mutableStateOf("") }
         var divisasData by remember { mutableStateOf<DivisaData?>(null) }
         var divisaOrigen by remember { mutableStateOf("Selecciona Origen") }
-        var divisaDestino by remember { mutableStateOf("") }
+        var divisaDestino by remember { mutableStateOf("Selecciona Destino") }
         var expandido by remember { mutableStateOf(false) }
-
+        var botonPresionado by remember { mutableStateOf("") }
 
         var divisaViewModel = DivisaViewModel()
         val apiKey = "XXXXX"
-        val baseCurrency = "MXN"
-        val currencies = "USD"
 
-        divisaViewModel.viewModelScope.launch(Dispatchers.IO){
-            divisasData = divisaViewModel.
-                getDivisas(apiKey)
-            Log.i("DIVISAS", divisasData.toString())
+
+        try {
+            divisaViewModel.viewModelScope.launch(Dispatchers.IO){
+                divisasData = divisaViewModel.
+                    getDivisas(apiKey)
+                Log.i("DIVISAS", divisasData.toString())
+            }
         }
-        divisaViewModel.viewModelScope.launch(Dispatchers.IO){
-            contenidoWebService = divisaViewModel.
-                getConversion(apiKey, baseCurrency, currencies).
-                data[currencies].toString()
+        catch (ex: Exception){
+
+        }
+        if(divisaOrigen.length == 3 && divisaDestino.length == 3) {
+            divisaViewModel.viewModelScope.launch(Dispatchers.IO) {
+                contenidoWebService = divisaViewModel.getConversion(
+                    apiKey,
+                    divisaOrigen,
+                    divisaDestino
+                ).data[divisaDestino].toString()
+            }
         }
         Column {
-            Button(onClick = { expandido = true }) {
+            Button(onClick = {
+                expandido = true
+                botonPresionado = "botonOrigen"
+            }) {
                 Text(text = divisaOrigen)
             }
 
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Selecciona Destino")
+            Button(onClick = {
+                expandido = true
+                botonPresionado = "botonDestino"
+            }) {
+                Text(text = divisaDestino)
             }
             Text(
                 text = contenidoWebService,
@@ -84,7 +102,12 @@ class MainActivity : ComponentActivity() {
                     DropdownMenuItem(
                         text = { Text(text = divisaItem.value.name )},
                         onClick = {
-                            divisaOrigen = divisaItem.value.code
+                            if(botonPresionado == "botonOrigen") {
+                                divisaOrigen = divisaItem.value.code
+                            }
+                            if (botonPresionado == "botonDestino") {
+                                divisaDestino = divisaItem.value.code
+                            }
                             expandido = false
                         })
                 }
